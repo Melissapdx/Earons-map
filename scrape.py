8e9191e379072060634f4f375d4d20073fdffa38
Melissa Orgill <orgillmelissa33@gmail.com>
2018-01-31 21:15:16 -0800

    finalized scrape script and generated data.json file

--- scrape.py
+++ scrape.py
@@ -18,16 +18,39 @@ def get_table_data(data):
 def convert_table(my_table):
     """ Pull TD from table and dump to json"""
     rows = my_table.findChildren(['th','tr'])
-
+    labels = ["date", "time", "location", "description", "name", "type"]
+    output = []
+    idx_row = 1
     for row in rows:
         cells = row.findChildren('td')
-        for cell in cells:
-            value = cell.string
-            attack_data = json.dumps(value)
-    return attack_data
+        # Filter out Things
+        # filter out if cells are empty
+        if len(cells) < 6:
+            continue
+        # if this type is an attack
+        type_of_attack = cells[5].string
+        # Filter out blank types
+        if type_of_attack is None:
+            continue
+        #filter out type of attack that are not attacks
+        if "Attack" not in type_of_attack:
+            continue
+        attack_data = {"id" : idx_row}
+        idx_row += 1
+        for idx, cell in enumerate(cells):
+            label = labels[idx]
+            #convert to string format, strip fails if cell.string is none so cast cell.string into a string
+            value = "{}".format(cell.string).strip()
+            attack_data[label] = value
+        output.append(attack_data)
+    return output
 
-
-
+def write_data_to_file(data):
+    # we have data, dump it into the json file.
+    json_data = json.dumps(data)
+    with open('data.json', 'w') as outfile:
+        outfile.write(json_data)
 
 table = get_table_data(results)
-convert_table(table)
+dictionay_of_attacks = convert_table(table)
+write_data_to_file(dictionay_of_attacks)
