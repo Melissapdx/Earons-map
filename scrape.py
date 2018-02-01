c69de81902fc2f94c6d873343527bdd729ed118f
Melissa Orgill <orgillmelissa33@gmail.com>
2018-01-31 16:15:58 -0800

    functions for data scraped from website complete

--- scrape.py
+++ scrape.py
@@ -1,16 +1,33 @@
 from bs4 import BeautifulSoup
 import requests
+import json
 # -*- coding: utf-8 -*-
-
-
 results = requests.get('https://www.coldcase-earons.com/timeline.php')
 
-#storeing content in variable then parse with beautiful soup
-c = results.content
-soup = BeautifulSoup(c)
+def get_table_data(data):
+    """ Retrieve data from website table of attacks with beautiful soup"""
+    #storing content in variable then parse with beautiful soup
+    c = results.content
+    soup = BeautifulSoup(c)
+    tables = soup.findChildren('table')
+    my_table = tables[1]
 
-sample = soup.find_all('td')
-print sample[0]
+    return my_table
 
-for table_data in soup.find_all('td'):
-    print table_data
+
+def convert_table(my_table):
+    """ Pull TD from table and dump to json"""
+    rows = my_table.findChildren(['th','tr'])
+
+    for row in rows:
+        cells = row.findChildren('td')
+        for cell in cells:
+            value = cell.string
+            attack_data = json.dumps(value)
+    return attack_data
+
+
+
+
+table = get_table_data(results)
+convert_table(table)
