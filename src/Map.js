bb5c1e69056b87f8397aa47542e165472fa0f03d
Melissa Orgill <orgillmelissa33@gmail.com>
2018-02-05 14:50:37 -0800

    added nav and map componenets

---
+++ src/Map.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class ShowMap extends React.Component {
+	render() {
+		return (
+				<div className="map">
+				</div>
+			);                
+    }
+}
+
+export default ShowMap;
---
+++ src/components/Navigation.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Navigation extends React.Component {
+	render() {
+		return (
+				<header className="navigation">
+					<h1 className="header">Earons</h1>
+					<ul className="menu">
+						<li>Home</li>
+						<li>About</li>
+						<li>Contact</li>
+					</ul>
+
+				</header>
+			);                
+    }
+}
+
+export default Navigation;
