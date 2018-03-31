9947754a000e9adc72dc03ef3432d7d610ff856e
Melissa Orgill <orgillmelissa33@gmail.com>
2018-03-30 18:57:20 -0700

    added typewriting effect

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,7 +4,9 @@ class Navigation extends React.Component {
 	render() {
 		return (
 				<header className="navigation">
+				<div class="typewriter">
 					<h1 className="header">Earons</h1>
+				</div>
 					<ul className="menu">
 						<li>Home</li>
 						<li>About</li>
